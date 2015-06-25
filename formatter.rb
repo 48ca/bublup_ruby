2bc1cddebad190908dd9380014f938879d8a1054
jamesthoughton <jamesthoughton@gmail.com>
2015-06-25 13:22:17 -0400

    Added comments to formatter.rb

--- formatter.rb
+++ formatter.rb
@@ -9,20 +9,20 @@ class Formatter
 		return @string.split #returns array of strings from string by spaces
 	end
 	def render(format,array)
-		ctrl = Controller.new(array)
-		template = ERB.new File.new("partials/"+format+".erb").read,nil,"%"
-		template.result(ctrl.getBinding)
+		ctrl = Controller.new(array) #pass control to Controller
+		template = ERB.new File.new("partials/"+format+".erb").read,nil,"%" #initialize template
+		template.result(ctrl.getBinding) #render and return template
 	end
 end
 class Controller
-	def initialize(array=nil)
+	def initialize(array=nil) #initialize values
 		@array = array
 		@maxLength = array.max_by{|x|x.length}.length
 	end
-	def getBinding
+	def getBinding #provide a method for ERB to get binding
 		binding
 	end
-	def ast
+	def ast #be able to format strings for asterisk format (called in partials/asterisk.erb)
 		@array.length.times do |element|
 			@array[element] = @array[element].ljust(@maxLength+1," ")+"*"
 			@array[element] = "*"+@array[element].rjust(@maxLength+3," ")
